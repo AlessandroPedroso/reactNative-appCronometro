9b95df5666c273f9e6cb1d28b82e16f8c105c32b
Your Name <alessanrospedroso@outlook.com>
2025-02-27 11:59:13 -0300

    finalizado

--- App.js
+++ App.js
@@ -1,57 +1,127 @@
-import { Text, View,StyleSheet,Image,TouchableOpacity } from "react-native";
+import {useState} from 'react';
+import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+
+let timer = null;
+let ss = 0;
+let mm = 0;
+let hh = 0;
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Image source={require('./src/crono.png')} />
+  const [numero, setNumero] = useState(0);
+  const [botao, setBotao] = useState('VAI');
+  const [ultimo, setUltimo] = useState(null);
 
-            <Text style={styles.timer}>00:00:00</Text>
+  function vai() {
+    if (timer !== null) {
+      //aqui vai parar o timer
+      clearInterval(timer);
+      timer = null;
+      setBotao('VAI');
+    } else {
+      //Começar a girar o timer
+      timer = setInterval(() => {
+        ss++;
 
-            <View style={styles.btnArea}>
-                <TouchableOpacity styles={styles.btn}>
-                    <Text style={styles.btnTexto}>VAI</Text>
-                </TouchableOpacity>
+        if (ss == 60) {
+          ss = 0;
+          mm++;
+        }
+        if (mm == 60) {
+          mm = 0;
+          hh++;
+        }
 
-                <TouchableOpacity styles={styles.btn}>
-                    <Text style={styles.btnTexto}>LIMPAR</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
+        let format =
+          (hh < 10 ? '0' + hh : hh) +
+          ':' +
+          (mm < 10 ? '0' + mm : mm) +
+          ':' +
+          (ss < 10 ? '0' + ss : ss);
+        setNumero(format);
+      }, 1000);
+      setBotao('PARAR');
+    }
+  }
+
+  function limpar() {
+    if (timer !== null) {
+      //Parar o timer!
+      clearInterval(timer);
+      timer = null;
+    }
+
+    setUltimo(numero);
+    setNumero(0);
+    ss = 0;
+    mm = 0;
+    hh = 0;
+    setBotao('VAI');
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image source={require('./src/crono.png')} />
+
+      <Text style={styles.timer}>{numero}</Text>
+
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={styles.btn} onPress={vai}>
+          <Text style={styles.btnTexto}>{botao}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn} onPress={limpar}>
+          <Text style={styles.btnTexto}>LIMPAR</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.areaUltima}>
+        <Text style={styles.textoCorrida}>
+          {ultimo ? `Ultimo tempo: ${ultimo}` : ''}
+        </Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#00AEEF'
-    },
-    timer: {
-        marginTop: -160,
-        fontSize: 45,
-        fontWeight: 'bold',
-        color:'#FFF'
-    },
-    btnArea: {
-    
-        flexDirection: 'row',
-        marginTop: 130,
-        height: 40,
-        backgroundColor:'red'
-    },
-    btn:{
-        flex:1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor:'#FFF',
-        height:40,
-        margin: 17,
-        borderRadius:9
-    },
-    btnTexto: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color:'#fff'
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00aeef',
+  },
+  timer: {
+    marginTop: -160,
+    fontSize: 45,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  btnArea: {
+    flexDirection: 'row',
+    marginTop: 130,
+    height: 'auto',
+  },
+  btn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 17,
+    borderRadius: 9,
+  },
+
+  btnTexto: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#00aeef',
+  },
+  areaUltima: {
+    marginTop: 35,
+  },
+  textoCorrida: {
+    fontSize: 25,
+    color: '#FFF',
+    fontStyle: 'italic',
+  },
+});
